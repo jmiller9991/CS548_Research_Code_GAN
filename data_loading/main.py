28a36cf4ed1d7ce1de1b3f3e482770f73c024656
Andrew <bertina@sunypoly.edu>
2020-09-30 13:36:41 -0400

    testing max iterations

--- data_loading/main.py
+++ data_loading/main.py
@@ -11,6 +11,11 @@ pathLittleBear = '/media/Data/CK+/CK+/cohn-kanade-images/S005'
 pathLocal = r'c:\Images\New Folder'
 
 for root, dirs, files in os.walk(pathLittleBear):
+    x=0
+    for directory in dirs:
+        if x == 2: 
+            break
+        x += 1
         for file in files:
             if file.endswith('.png'):
                 pngFiles.append(os.path.join(root, file))
