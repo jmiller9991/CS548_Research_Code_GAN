fa3e3280d102e2bed4852cb96713dbffaad1742d
jmiller9991 <jacobmiller9991@gmail.com>
2020-10-09 18:54:08 -0400

    Made message todos

--- stylegan2/run_projector.py
+++ stylegan2/run_projector.py
@@ -33,7 +33,7 @@ def project_image(proj, targets, png_prefix, num_snapshots):
         if proj.get_cur_step() in snapshot_steps:
             print('Cond Reached')
             sub_dalatents = proj.get_dlatents()
-            np.save(proj.get_images(), png_prefix + 'step%04d.png' % proj.get_cur_step(), sub_dalatents)
+            np.save(png_prefix + 'step%04d.png' % proj.get_cur_step(), sub_dalatents)
             misc.save_image_grid(proj.get_images(), png_prefix + 'step%04d.png' % proj.get_cur_step(), drange=[-1,1])
             print('Saved Step Image')
         dalatents = proj.get_dlatents()
