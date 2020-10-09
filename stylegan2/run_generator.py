89f46ed3c7426c67786f979fd57056c141fb7730
jmiller9991 <jacobmiller9991@gmail.com>
2020-10-09 14:22:15 -0400

    Made message todos

--- stylegan2/run_generator.py
+++ stylegan2/run_generator.py
@@ -16,7 +16,7 @@ import pretrained_networks
 
 #----------------------------------------------------------------------------
 
-#TODO Run a projector on the generator to get the latent space of recently generated image
+#TODO FAR FUTURE MAYBE Run a projector on the generator to get the latent space of recently generated image
 
 def generate_images(network_pkl, seeds, truncation_psi):
     print('Loading networks from "%s"...' % network_pkl)
