8d98cf2cb4e5db1ed1a765f618612d29ee26168a
jmiller9991 <jacobmiller9991@gmail.com>
2020-11-29 20:05:59 -0500

    Corrected imports

--- Model.py
+++ Model.py
@@ -71,3 +71,7 @@ def main():
 
     history_latent = model_latent.fit(data, facs, batch_size=batch_size_latents, epochs=epochs_latents)
     history_image = modelImage.fit(images, facs, batch_size=batch_size_image, epochs=epochsImage)
+
+
+if __name__ == "__main__":
+    main()
