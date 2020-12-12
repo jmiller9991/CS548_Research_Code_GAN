288b11ff1b56197175368adc71c747dda1c44ea8
AndrewBert <bertinoandrew11@gmail.com>
2020-12-11 19:38:01 -0500

    reverted fix to facsdata, moved to local branch

--- Data/ck.py
+++ Data/ck.py
@@ -37,6 +37,51 @@ def getEmotionData():
 @return Two numpy arrays: one for the facslabels from each sequence and one for 
     the total sum of each action unit when present. 
 """
+def getFacsData(selectedActionUnits = []):
+    allActionUnits = np.array([1,2,4,5,6,7,9,10,11,12,13,14,15,16,17,18,20,21,23,24,25,26,27,28,29,31,34,38,39,43])
+    if not selectedActionUnits:
+        selectedActionUnits = allActionUnits
+    sumOfActionUnits = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    intensityOfActionUnits = {}
+    facsLabels = []
+    for subject in sorted(os.listdir(facsPath)):
+        subjectPath = os.path.join(facsPath,subject)
+        if os.path.isdir(subjectPath):
+            for sequence in sorted(os.listdir(subjectPath)):
+                sequencePath = os.path.join(subjectPath,sequence)
+                if os.path.isdir(sequencePath):
+                    allFacsLabels = []
+                    facsFilePaths = []
+                    for facsFile in sorted(os.listdir(sequencePath)):
+                        path = os.path.join(sequencePath, facsFile)
+                        facsFilePaths.append(path)
+                    sequenceFacsLabels = np.zeros(shape=30)
+                    facsFile = open(facsFilePaths[-1], "r")
+                    for line in facsFile:
+                        for i, sequenceActionUnit in enumerate(line.split()):
+                            #get the au
+                            if i == 0:
+                                sequenceActionUnitInt = float(sequenceActionUnit)
+                                if sequenceActionUnitInt in selectedActionUnits:
+                                    j = 0
+                                    for au in allActionUnits:
+                                        if sequenceActionUnitInt == au:
+                                            sequenceFacsLabels[j] = 1
+                                            sumOfActionUnits[j] += 1
+                                            intensityOfActionUnits[au] = 0
+                                        j+=1
+                            #get the intensity
+                            if i == 1:
+                                intensityOfActionUnits[au] = 0
+                    facsLabels.append(sequenceFacsLabels)
+    return np.asarray(facsLabels), np.asarray(sumOfActionUnits)
+
+"""
+@selectedActionUnits: An optional list of the desired action units to track. 
+    If no list is passed in, allActionUnits will be selected.
+@return Two numpy arrays: one for the facslabels from each sequence and one for 
+    the total sum of each action unit when present. 
+"""
 def getFacsDataWithoutIntensity(selectedActionUnits = []):
     allActionUnits = np.array([1,2,4,5,6,7,9,10,11,12,13,14,15,16,17,18,20,21,23,24,25,26,27,28,29,31,34,38,39,43])
     if not selectedActionUnits:
